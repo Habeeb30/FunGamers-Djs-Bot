c7c41ed549cb5e661254610dfa2abbb840ad7ba2
Habeebur Rahman <78295296+Habeeb30@users.noreply.github.com>
2022-09-22 13:23:56 +0530

    Update

--- Events/Client/Ready.js
+++ Events/Client/Ready.js
@@ -1,7 +1,5 @@
-const fs = require("fs");
 const chalk = require("chalk");
 const { loadCommands } = require("../../Handlers/commandHandler");
-// const Handlers = fs.readdirSync("./Handlers");
 const { Client, ActivityType } = require("discord.js");
 const mongoose = require("mongoose");
 const { Database_URI } = process.env;
@@ -18,17 +16,9 @@ module.exports = {
     console.log(chalk.yellow(`✅ >>> Ready! Logged in as ${client.user.tag}`));
 
     loadCommands(client);
-    // for (const folder of Handlers) {
-    //   const Handlers = fs
-    //     .readdirSync(`../../Handlers/${folder}`)
-    //     .filter((file) => file.endsWith(".js"));
-    //   for (const file of Handlers) {
-    //     require(`../../Handlers/${folder}/${file}`)(client);
-    //   }
-    // }
 
     client.user.setPresence({
-      activities: [{ name: "First mention me", type: ActivityType.Listening }],
+      activities: [{ name: "/help", type: ActivityType.Listening }],
       status: "dnd",
     });
 
