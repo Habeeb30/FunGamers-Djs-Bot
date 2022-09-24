b1ebae8f94d3157a39c4fa300e404656e6e699a3
Habeebur Rahman <78295296+Habeeb30@users.noreply.github.com>
2022-09-24 23:56:05 +0530

    Added Avatar Context

---
+++ Commands/Context/avatar.js
@@ -0,0 +1,36 @@
+const {
+  Client,
+  ContextMenuCommandBuilder,
+  ApplicationCommandType,
+} = require("discord.js");
+
+module.exports = {
+  data: new ContextMenuCommandBuilder()
+    .setName("Avatar")
+    .setType(ApplicationCommandType.User),
+  category: "Context",
+
+  /**
+   * @param {ContextMenuCommandInteraction} interaction
+   * @param {Client} client
+   */
+  async execute(interaction, client) {
+    await interaction.deferReply({ ephemeral: true });
+
+    const { guild, targetId } = interaction;
+
+    const target = await guild.members.cache.get(targetId);
+
+    const Embed = new EmbedBuilder()
+      .setColor(client.color)
+      .setAuthor({
+        name: `${target.user.username}'s Avatar`,
+        iconURL: target.user.displayAvatarURL(),
+      })
+      .setImage(target.user.displayAvatarURL({ size: 512 }))
+      .setFooter({ text: "Avatar by FunGamers" })
+      .setTimestamp();
+
+    return interaction.editReply({ embeds: [Embed] });
+  },
+};
